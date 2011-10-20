39c9493d453f8d2668abab16ec7ae4f960830df3
Matt Hidinger <1432195+matthidinger@users.noreply.github.com>
2011-10-20 19:13:21 +0200

    Made it pull data from Yahoo Finance API.

---
+++ src/RealWorldStocks.Web/Extensions/StringExtensions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text;
+
+namespace RealWorldStocks.Web.Extensions
+{
+    public static class StringExtensions
+    {
+        // Taken from Chris Howie (http://www.chrishowie.com/) in his answer on Stack Overflow.
+        // Source: http://stackoverflow.com/questions/4150697/regular-expression-to-split-on-comma-except-if-quoted 
+        public static IEnumerable<string> SplitCSV(this string csvString)
+        {
+            var sb = new StringBuilder();
+            bool quoted = false;
+
+            foreach (char c in csvString)
+            {
+                if (quoted)
+                {
+                    if (c == '"')
+                        quoted = false;
+                    else
+                        sb.Append(c);
+                }
+                else
+                {
+                    if (c == '"')
+                    {
+                        quoted = true;
+                    }
+                    else if (c == ',')
+                    {
+                        yield return sb.ToString();
+                        sb.Length = 0;
+                    }
+                    else
+                    {
+                        sb.Append(c);
+                    }
+                }
+            }
+
+            if (quoted)
+                throw new ArgumentException("csvString", "Unterminated quotation mark.");
+
+            yield return sb.ToString();
+        }
+    }
+}
