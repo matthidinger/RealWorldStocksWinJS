70901b3c6d5fecdbaae6f7b20729d1d3eb457555
mhidinger <mhidinger@localhost>
2012-05-06 14:01:42 -0500

    - Adding Windows phone and WinJS to this source code - Moving the original root into WindowsPhone folder - Adding WinJS folder to support the WinJS code

---
+++ WinJS/src/RealWorldStocks.Windows.Client/js/services.js
@@ -0,0 +1,36 @@
+﻿(function () {
+    "use strict";
+
+    WinJS.Namespace.define("RealWorldStocks", {
+
+        Services: WinJS.Class.define(null, {
+
+            getQuotes: function (symbols) {
+                /// <summary>
+                /// Gets the most recent quotes for the symbols
+                /// </summary>
+                /// <param name="symbols" type="Array">
+                /// The array of symbols 
+                /// </param>
+
+                var url = "http://finance.yahoo.com/d/quotes.csv?s=" + symbols.join() + "&f=snol1vpc1p2ghyra2j1abe8";
+
+                return WinJS.xhr({
+                    url: url
+                }).then(function (response) {
+                    var snapshots = [];
+
+                    var data = RealWorldStocks.Utils.csvToArray(response.responseText);
+                    data.forEach(function (item) {
+                        var snapshot = RealWorldStocks.Models.StockSnapshot.parse(item);
+                        snapshots.push(snapshot);
+                    });
+
+                    return snapshots;
+                });
+            },
+
+        })
+
+    });
+})();
