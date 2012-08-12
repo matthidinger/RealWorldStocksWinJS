be7066a710fa2d847ebb4196de24ad706a477f5b
Matt <Matt@LEGACY>
2012-08-12 13:14:45 -0500

    - Converted WinJS to RP, should be working, but found some styling bugs that will be resolved later

---
+++ WinJSRP/src/RealWorldStocks.Windows/models/appState.js
@@ -0,0 +1,89 @@
+﻿(function () {
+    "use strict";
+    var services = new RealWorldStocks.Services();
+    var localSettings = Windows.Storage.ApplicationData.current.roamingSettings;
+
+    var defaults = [
+        new RealWorldStocks.Models.StockSnapshot("MSFT"),
+        new RealWorldStocks.Models.StockSnapshot("AAPL"),
+        new RealWorldStocks.Models.StockSnapshot("GOOG"),
+        new RealWorldStocks.Models.StockSnapshot("NOK"),
+        new RealWorldStocks.Models.StockSnapshot("T"),
+        new RealWorldStocks.Models.StockSnapshot("F"),
+        new RealWorldStocks.Models.StockSnapshot("GE"),
+        new RealWorldStocks.Models.StockSnapshot("XOM"),
+        new RealWorldStocks.Models.StockSnapshot("NFLX"),
+    ];
+
+    var watchList = new WinJS.Binding.List([], {
+        binding: true
+    });
+
+    watchList.findSnapshot = function (symbol) {
+        /// <returns type="RealWorldStocks.Models.StockSnapshot" />
+        var found = this.filter(function (item) { return item.symbol === symbol });
+        if (found.length > 0) {
+            return found[0];
+        }
+        return null;
+    };
+
+    watchList.refresh = function () {
+        var that = this;
+        var symbols = this.map(function (item) {
+            return item.symbol;
+        });
+
+        services.getQuotes(symbols).then(function (snapshots) {
+            snapshots.forEach(function (snapshot) {
+                RealWorldStocks.Utils.extend(that.findSnapshot(snapshot.symbol), snapshot);
+            });
+        });
+        
+    };
+
+    WinJS.Namespace.define("RealWorldStocks.AppState", {
+
+        watchList: watchList,
+
+        persist: function () {
+            return new WinJS.Promise(function (complete) {
+
+                var raw = [];
+                watchList.forEach(function (item) {
+                    raw.push(item.backingData);
+                });
+
+                //var json = JSON.stringify(watchList);
+                //localSettings.values["watchList"] = json;
+                localSettings.values["watchList"] = JSON.stringify(raw);
+                complete();
+            });
+
+        },
+
+
+        restore: function () {
+            return new WinJS.Promise(function (complete) {
+
+                if (localSettings.values.hasKey("watchList")) {
+                    var raw = JSON.parse(localSettings.values["watchList"]);
+                    raw.forEach(function (item) {
+                        watchList.push(item);
+                    });
+                } else {
+                    defaults.forEach(function (item) {
+                        watchList.push(item);
+                    });
+                }
+
+                complete();
+            });
+
+        },
+
+
+    });
+
+})();
+ 
---
+++ WinJSRP/src/RealWorldStocks.Windows/models/stockSnapshot.js
@@ -0,0 +1,41 @@
+﻿(function () {
+    "use strict";
+    
+    WinJS.Namespace.define("RealWorldStocks.Models", {
+
+        StockSnapshot: WinJS.Class.define(function (symbol) {
+            /// <param name="symbol" type="String">
+            /// The symbol this snapshot is for
+            /// </param>
+
+
+            // All fields must be defined for automatic WinJS.Binding.as() support
+            this.symbol = symbol;
+            this.company = null;
+            this.openingPrice = 0.0;
+            this.lastPrice = 0.0;
+            this.volume = 0;
+            this.previousClose = 0.0;
+            this.daysChange = 0.0;
+            this.daysChangePercent = 0.0;
+        }, {
+
+        }, {
+            parse: function (csv) {
+                /// <param name="csv" type="Array">
+                /// The array of CSV data
+                /// </param>
+                /// <returns type="RealWorldStocks.Models.StockSnapshot" />
+                var snapshot = new RealWorldStocks.Models.StockSnapshot(csv[0]);
+                snapshot.company = csv[1];
+                snapshot.openingPrice = parseFloat(csv[2]);
+                snapshot.lastPrice = parseFloat(csv[3]);
+                snapshot.volume = parseInt(csv[4]);
+                snapshot.previousClose = parseFloat(csv[5]);
+                snapshot.daysChange = parseFloat(csv[6]);
+                snapshot.daysChangePercent = parseFloat(csv[7]);
+                return snapshot;
+            }
+        })
+    });
+})();
